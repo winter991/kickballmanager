fda035b5f736a12b9b3011b8440adc2f2384fe6b
Jason Esposito <jayespo991@gmail.com>
2015-02-08 11:34:25 -0500

    aaa

---
+++ KickballManager/Models/LineupDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace KickballManager.Models
+{
+    public class LineupDetails
+    {
+        [Key]
+        public int LineupDetailID { get; set; }
+        public bool hasKicked { get; set; }
+        public bool hasFielded { get; set; }
+        public int PlayerID { get; set; }
+        public int LineupID { get; set; }
+        public virtual Lineup Lineup { get; set; }
+        public virtual Player Player { get; set; }
+
+    }
+}
